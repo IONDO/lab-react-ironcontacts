f35227f6fba256ae98a85ad89f7af7c3bb44eddd
iondo <ines.ondo.baka@gmail.com>
2019-03-23 19:02:15 +0100

    sort + delete

--- src/App.js
+++ src/App.js
@@ -15,15 +15,39 @@ class App extends Component {
     this.setState({
       actors:[...this.state.actors, randomActor]
     })
-    console.log(this.state.actors)
   }
 
-  sortContact = () =>{
-    console.log(this.state.actors)
+  sortContact = () => {
+     this.state.actors.sort( (actor1,actor2) => 
+      actor1.name.localeCompare(actor2.name)
+    );
+    this.setState({
+      actors:this.state.actors
+    })
   }
 
-  sortContactByPopularity = () =>{
-    console.log(this.state.actors)
+  sortContactByPopularity = () => {
+     this.state.actors.sort( (actor1,actor2) => {
+      if (actor1.popularity < actor2.popularity) {
+        return 1;
+      }
+      if (actor1.popularity > actor2.popularity) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    })
+    this.setState({
+      actors:this.state.actors
+    })
+  }
+
+  deleteContact = (index) => {
+    this.state.actors.splice(index,1);
+    this.setState({
+     actors: this.state.actors
+    })
+    console.log(this.state.actors);
   }
 
   render() {
@@ -32,7 +56,7 @@ class App extends Component {
         <h2>IronhackContacts</h2>
         <button onClick={this.addContact}>Add Random Actor</button>
         <button onClick={this.sortContact}>Sort by Name</button>
-        <button onClick={this.addContact}>Sort by Polularity</button>
+        <button onClick={this.sortContactByPopularity}>Sort by Polularity</button>
         <ul>
             {this.state.actors.map((actor, index) => {
             return <Famous
@@ -41,6 +65,7 @@ class App extends Component {
               image={actor.pictureUrl}
               name={actor.name}
               popularity={actor.popularity}
+              contactDeleted={this.deleteContact}
             />
             })}  
         </ul>
--- src/Components/Famous.js
+++ src/Components/Famous.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 
 class Famous extends Component {
 
-  addingContact = () => {
-    return this.props.contactAdded;
+  deleteContact = () => this.props.contactDeleted(this.props.index)
+
+  render() {
+    return (
+      <ul>
+        <li>
+          <img src={this.props.image} alt=''/>
+          <p>{this.props.name}</p>
+          <p>{this.props.popularity}</p>
+          <button onClick={this.deleteContact}>Delete</button>
+        </li>
+      </ul>
+    );
   }
-    render() {
-      return (
-        <ul>
-          <li>
-            <img src={this.props.image} alt=''/>
-            <p>{this.props.name}</p>
-            <p>{this.props.popularity}</p>
-          </li>
-        </ul>
-      );
-    }
-  }
-  
-  export default Famous;
+}
+
+export default Famous;
